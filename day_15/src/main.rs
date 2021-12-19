d42a0dfe35d51b69b258cfc49c4adeca1c603f21
Marcel Robitaille <mail@marcelrobitaille.me>
2021-12-18 23:59:14 -0500

    Update day 15 with priority queue
    Also replace the horrible recursive thing with an iterative
    implementation.
    
    I like immutability, but I also like getting the answer without a stack
    overflow. I wish it would use tail call optimization, but I guess not.

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,9 +1,10 @@
 use itertools::iproduct;
+use priority_queue::PriorityQueue;
+use std::cmp::Reverse;
 use std::collections::{HashMap, HashSet};
 use std::env;
 use std::hash::Hash;
 use std::io::{stdin, BufRead};
-use std::ops::BitOr;
 use std::process::exit;
 use termion::{color, style};
 use thiserror::Error;
@@ -61,35 +62,19 @@ impl Point {
     }
 }
 
-#[derive(Debug)]
-struct ExtensibleHashMap<K, V> {
-    base: HashMap<K, V>,
-}
-
-impl<K, V> BitOr<HashMap<K, V>> for ExtensibleHashMap<K, V>
-where
-    K: Hash + Eq,
-{
-    type Output = ExtensibleHashMap<K, V>;
-    fn bitor(self, rhs: HashMap<K, V>) -> Self::Output {
-        ExtensibleHashMap {
-            base: self.base.into_iter().chain(rhs).collect(),
-        }
-    }
-}
-
-fn unwind(source: Point, target: Point, parent: HashMap<Point, Point>) -> Vec<Point> {
-    fn recurse(current: Point, source: Point, parent: HashMap<Point, Point>) -> Vec<Point> {
+fn unwind(source: Point, target: Point, prev: HashMap<Point, Point>) -> Vec<Point> {
+    // Get the path from the prev map
+    fn recurse(current: Point, source: Point, prev: HashMap<Point, Point>) -> Vec<Point> {
         if current == source {
             return vec![source];
         }
-        recurse(*parent.get(&current).unwrap(), source, parent)
+        recurse(*prev.get(&current).unwrap(), source, prev)
             .into_iter()
             .chain([current].into_iter())
             .collect()
     }
 
-    recurse(target, source, parent)
+    recurse(target, source, prev)
 }
 
 fn dijkstra(
@@ -98,57 +83,69 @@ fn dijkstra(
     extent: Point,
     target: Point,
 ) -> Vec<Point> {
-    fn recurse(
-        q: HashSet<Point>,
-        dists: ExtensibleHashMap<Point, u32>,
-        parent: ExtensibleHashMap<Point, Point>,
-        seen: HashSet<Point>,
-        weights: &HashMap<Point, u32>,
-        extent: Point,
-        target: Point,
-    ) -> ExtensibleHashMap<Point, Point> {
-        let u = *q.iter().min_by_key(|u| dists.base.get(u)).unwrap();
-        if u == target {
-            return parent;
+    // Dijkstra's algorithm where we're allowed to visit directly adjacent cells in the grid
+    // More mutable than I'd like, but my previous recursive function caused a stack overflow
+    // even though it looked like it could have used tail recursion
+
+    // Set up priority queue and seed it with source cell
+    let mut q = PriorityQueue::<Point, Reverse<u32>>::new();
+    q.push(source, Reverse(0));
+
+    // Distance of each cell from the source
+    // Gets updated as we find better ways to get to each cell
+    let mut dists = HashMap::from([(source, 0)]);
+
+    // List of visisted nodes
+    let mut seen = HashSet::new();
+
+    // prev of each node
+    // How we can discover the path once we find a solution
+    let mut prev = HashMap::<Point, Point>::new();
+
+    while !q.is_empty() {
+        // Get closest (highest priority) node in queue
+        let (current, _priority) = q.pop().unwrap();
+        seen.insert(current);
+
+        // We're done!
+        if current == target {
+            break;
         }
 
-        let dist_u = dists.base.get(&u).unwrap_or(&u32::MAX);
+        // Save to current node
+        let current_dist = *dists.get(&current).unwrap_or(&u32::MAX);
 
-        let updated = [u.top(), u.left(), u.right(), u.bottom()]
-            .iter()
-            .filter_map(|v| *v)
-            .filter(|v| v.x < extent.x && v.y < extent.y)
-            .filter(|v| !seen.contains(v))
-            .filter_map(|v| {
-                let dist_v = dists.base.get(&v).unwrap_or(&u32::MAX);
-                let alt = dist_u + weights.get(&u).unwrap();
-                if alt < *dist_v {
-                    Some((v, alt, u))
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>();
+        // Check all neighbours
+        for neighbour in [
+            current.top(),
+            current.left(),
+            current.right(),
+            current.bottom(),
+        ]
+        .iter()
+        .filter_map(|v| *v)
+        // Ensure neighbour on grid
+        .filter(|v| v.x < extent.x && v.y < extent.y)
+        // Ensure we haven't visited neighbour already
+        .filter(|v| !seen.contains(v))
+        {
+            let neighbour_dist = dists.get(&neighbour).unwrap_or(&u32::MAX);
 
-        let q = &q | &HashSet::from_iter(updated.iter().map(|x| x.0));
-        let q = &q - &HashSet::from([u]);
+            // If it would be quicker to get to neighbour from current node,
+            // then update the distance and prev of v
+            let dist_to_neighbour_through_current = current_dist + weights.get(&current).unwrap();
+            if dist_to_neighbour_through_current < *neighbour_dist {
+                dists.insert(neighbour, dist_to_neighbour_through_current);
+                prev.insert(neighbour, current);
+            }
 
-        let seen = &seen | &HashSet::from([u]);
-        let dists = dists | HashMap::from_iter(updated.iter().map(|x| (x.0, x.1)));
-        let parent = parent | HashMap::from_iter(updated.iter().map(|x| (x.0, x.2)));
-        recurse(q, dists, parent, seen, weights, extent, target)
+            // Enqueue neighbour
+            q.push(neighbour, Reverse(*dists.get(&neighbour).unwrap()));
+        }
     }
 
-    let q = HashSet::from([source]);
-    let dists = ExtensibleHashMap::<Point, u32> {
-        base: HashMap::from([(source, 0)]),
-    };
-    let seen = HashSet::new();
-    let parent = ExtensibleHashMap {
-        base: HashMap::new(),
-    };
-    let parent = recurse(q, dists, parent, seen, weights, extent, target);
-    unwind(source, target, parent.base)
+    // Get the path from the prev map
+    unwind(source, target, prev)
 }
 
 fn print(extent: Point, path: &[Point], weights: &HashMap<Point, u32>) {
@@ -170,7 +167,7 @@ fn print(extent: Point, path: &[Point], weights: &HashMap<Point, u32>) {
     }
 }
 
-fn day_13() -> Result<u32, AdventError> {
+fn day_15() -> Result<u32, AdventError> {
     let args: Vec<String> = env::args().collect();
     let command = args.get(1).ok_or(AdventError::NoPartArgument)?;
     let question_part = match &command[..] {
@@ -249,7 +246,7 @@ fn day_13() -> Result<u32, AdventError> {
 }
 
 fn main() {
-    match day_13() {
+    match day_15() {
         Ok(answer) => println!("{}", answer),
         Err(err) => {
             eprintln!("{}", err);
